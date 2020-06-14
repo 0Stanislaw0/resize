1d8bc7770a29782c7a9e587c4a446390930390aa
Stanislaw <52160413+0Stanislaw0@users.noreply.github.com>
2020-06-14 16:58:05 +0400

    Update yasg.py

--- resize_image/image/yasg.py
+++ resize_image/image/yasg.py
@@ -3,6 +3,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path
 
+
 schema_view = get_schema_view(
    openapi.Info(
       title="API",
@@ -15,7 +16,16 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+post_resize = openapi.Schema(
+    type=openapi.IN_FORM,
+    required=['image_file','height','width'],
+    example={
+        'image_file': 'image.jpeg',
+        'height': 128,
+        'width': 128,
+    }
+)
+
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
